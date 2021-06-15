1c571c5f460e64a1d8606c3df66e419e41f0fd8a
Warren <warren@waza.co.za>
2021-06-15 08:00:38 +0200

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -34,6 +34,55 @@ resource "azurerm_subnet" "subnet_1" {
   address_prefix       = "10.0.1.0/24"
 }
 
+resource "azurerm_public_ip" "public_ip_1" {
+  name                = "virtual_network_gateway_public_ip_1"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  # Public IP needs to be dynamic for the Virtual Network Gateway
+  # Keep in mind that the IP address will be "dynamically generated" after
+  # being attached to the Virtual Network Gateway below
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_public_ip" "public_ip_2" {
+  name                = "virtual_network_gateway_public_ip_2"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  # Public IP needs to be dynamic for the Virtual Network Gateway
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_virtual_network_gateway" "virtual_network_gateway" {
+  name                = "virtual_network_gateway"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  # Configuration for high availability
+  active_active = true
+  # This might me expensive, check the prices  
+  sku           = "VpnGw1"
+
+  # Configuring the two previously created public IP Addresses
+  ip_configuration {
+    name                          = azurerm_public_ip.public_ip_1.name
+    public_ip_address_id          = azurerm_public_ip.public_ip_1.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.subnet_gateway.id
+  }
+
+  ip_configuration {
+    name                          = azurerm_public_ip.public_ip_2.name
+    public_ip_address_id          = azurerm_public_ip.public_ip_2.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.subnet_gateway.id
+  }
+}
+
 # The subnet where the VPN tunnel will live
 resource "azurerm_subnet" "subnet_gateway" {
   # The name "GatewaySubnet" is mandatory
